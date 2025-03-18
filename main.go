6b872f3c22e134991205163e72478f4e63136514
Martin Mackintosh <martindmackintosh@outlook.com>
2025-03-18 15:08:01 +0000

    add pausing and speed control

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"image/color"
 	_ "image/png"
 	"log"
+	"math"
 	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -24,6 +25,8 @@ type Game struct {
 	UserGen    line
 	Drawing    bool
 	HasWrapped bool
+	Paused     bool
+	TempImage  imageTimeout
 }
 
 type line struct {
@@ -34,60 +37,61 @@ type line struct {
 	Width float32
 }
 
-type timeout struct {
-	Image *ebiten.Image
+type imageTimeout struct {
+	Image     *ebiten.Image
 	TicksLeft uint
 }
 
 var (
-	gravDisabled       *bool
-	ugc        *bool
-	space      *cp.Space
-	ballArray  [500]*cp.Body
-	shapeArray []*cp.Shape
-	gravImages [6]*ebiten.Image
-	mass       float64
-	moment     float64
-	counter    uint64 // Should be used when drawing
-	writer     uint64 // Should be used when simulating
-	obst       []*cp.Shape
-	radius     float64
-	lines      []line
-	actor      *ebiten.Image
-	imgMode    bool
-	autonomous *bool
-	debugging  *bool
-	icon       []image.Image
-	gravDisplay timeout
+	gravDisabled *bool
+	ugc          *bool
+	space        *cp.Space
+	ballArray    [500]*cp.Body
+	shapeArray   []*cp.Shape
+	gravImages   [6]*ebiten.Image
+	pauseImg     [2]*ebiten.Image
+	mass         float64
+	moment       float64
+	counter      uint64 // Should be used when drawing
+	writer       uint64 // Should be used when simulating
+	obst         []*cp.Shape
+	radius       float64
+	lines        []line
+	actor        *ebiten.Image
+	imgMode      bool
+	autonomous   *bool
+	debugging    *bool
+	icon         []image.Image
+	speedImg     [2]*ebiten.Image
 )
 
 //go:embed data
 var fs embed.FS
 
-func obstGen(x0, y0, x1, y1 float64, visible bool) {
+func obstGen(x0, y0, x1, y1, r float64, visible bool) {
 	if visible {
 		var z line
-		z.X0, z.Y0, z.X1, z.Y1, z.Width = float32(x0), float32(y0), float32(x1), float32(y1), 4.0
+		z.X0, z.Y0, z.X1, z.Y1, z.Width = float32(x0), float32(y0), float32(x1), float32(y1), float32(r)
 		lines = append(lines, z)
 	}
 	obst = append(obst, cp.NewSegment(space.StaticBody, cp.Vector{X: x0, Y: y0}, cp.Vector{X: x1, Y: y1}, 2))
 }
 
 func loadImage(path string) *ebiten.Image {
-	x, _, err := ebitenutil.NewImageFromFileSystem(fs,path)
+	x, _, err := ebitenutil.NewImageFromFileSystem(fs, path)
 	if err == nil {
 		return x
 	} else {
-		log.Fatalln("Failed to load image with path " + path + "; error:\n",err)
+		log.Fatalln("Failed to load image with path "+path+"; error:\n", err)
 		return nil
 	}
 }
 func loadMultiple(paths []string) []*ebiten.Image {
 	var images []*ebiten.Image
 	for n, i := range paths {
-		x, _, err := ebitenutil.NewImageFromFileSystem(fs,i)
+		x, _, err := ebitenutil.NewImageFromFileSystem(fs, i)
 		if err != nil {
-			log.Fatalf("Loading images failed! Image no. %d, path %s\n",n,i)
+			log.Fatalf("Loading images failed! Image no. %d, path %s\n", n, i)
 		}
 		images = append(images, x)
 	}
@@ -96,34 +100,50 @@ func loadMultiple(paths []string) []*ebiten.Image {
 
 func init() {
 	var err error
-	gravDisplay.TicksLeft = 0
+	// Load actor (mario coin)
 	actor = loadImage("data/actor.png")
+	// Load window icon
 	_, iconImage, err := ebitenutil.NewImageFromFileSystem(fs, "data/icon.png")
 	if err != nil {
 		log.Fatalln("Failed to load logo")
 	}
-	images := loadMultiple([]string{"data/xgravadd.png","data/xgravsub.png","data/ygravadd.png","data/ygravsub.png","data/gravreset.png","data/gravlimit.png"})
-	copy(gravImages[:],images[:])
+
+	// Load gravity control messages, copy them into array
+	images := loadMultiple([]string{"data/xgravadd.png", "data/xgravsub.png", "data/ygravadd.png", "data/ygravsub.png", "data/gravreset.png", "data/gravlimit.png"})
+	copy(gravImages[:], images[:])
+
+	// Load pause messages, copy them into array
+	pauseDialogues := loadMultiple([]string{"data/paused.png", "data/resumed.png"})
+	copy(pauseImg[:], pauseDialogues[:])
+
+	// Load speed control messages, copy them into array
+	speedImgSlice := loadMultiple([]string{"data/speedup.png", "data/slowdown.png"})
+	copy(speedImg[:], speedImgSlice[:])
+
+	// Append icon image to slice
 	icon = append(icon, iconImage)
+
+	// Create physics simulation space and set gravity
 	space = cp.NewSpace()
 	space.SetGravity(cp.Vector{X: 0.0, Y: 300.0})
-	obstGen(160, 100, 320, 60, true)
-	obstGen(320, 60, 480, 100, true)
-	obstGen(0, 140, 160, 180, true)
-	obstGen(640, 140, 480, 180, true)
-	obstGen(0, 0, 0, 0x300, false)
-	obstGen(0x280, 0, 0x280, 0x300, false)
-	obstGen(0, -5, 0x280, -5, false)
-	obstGen(0, 240, 500, 300, true)
-	obstGen(640, 400, 140, 460, true)
-	obstGen(0, 520, 300, 600, true)
-	obstGen(640, 520, 340, 600, true)
-
+	// Add obstacles and set properties
+	obstGen(160, 100, 320, 60, 4.0, true)
+	obstGen(320, 60, 480, 100, 4.0, true)
+	obstGen(0, 140, 160, 180, 4.0, true)
+	obstGen(640, 140, 480, 180, 4.0, true)
+	obstGen(-2, 0, -2, 0x300, 10.0, false)
+	obstGen(0x282, 0, 0x282, 0x300, 10.0, false)
+	obstGen(0, -5, 0x280, -5, 4.0, false)
+	obstGen(0, 240, 500, 300, 4.0, true)
+	obstGen(640, 400, 140, 460, 4.0, true)
+	obstGen(0, 520, 300, 600, 4.0, true)
+	obstGen(640, 520, 340, 600, 4.0, true)
 	for _, x := range obst {
 		x.SetFriction(1)
 		x.SetElasticity(0.25)
 		space.AddShape(x)
 	}
+	// Generate first ball and set some properties
 	radius = 8.0
 	mass = 1.0
 	moment = cp.MomentForCircle(mass, 0, radius, cp.Vector{X: 0, Y: 0})
@@ -136,13 +156,29 @@ func init() {
 	writer = 1
 }
 
+// Runs every tick.
+// Welcome to the land of the if statements.
 func (g *Game) Update() error {
+	// Pause the game if user strikes key K
+	if inpututil.IsKeyJustPressed(ebiten.KeyK) {
+		switch g.Paused {
+		case true:
+			g.Paused = false
+			g.TempImage.Image = pauseImg[1]
+			g.TempImage.TicksLeft = 30
+		case false:
+			g.Paused = true
+			g.TempImage.Image = pauseImg[0]
+			g.TempImage.TicksLeft = math.MaxUint
+		}
+	}
 	var auto bool = g.Inputless >= 900
 	if *autonomous {
 		auto = true
 	}
-	if gravDisplay.TicksLeft != 0 {
-		gravDisplay.TicksLeft--
+	// Reduce time remaining for temporary dialogues
+	if g.TempImage.TicksLeft != 0 {
+		g.TempImage.TicksLeft--
 	}
 	var releasedTouches = inpututil.AppendJustReleasedTouchIDs([]ebiten.TouchID{})
 	var touch bool = false
@@ -155,35 +191,47 @@ func (g *Game) Update() error {
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyA) && !*gravDisabled {
 		space.SetGravity(space.Gravity().Sub(cp.Vector{X: 50, Y: 0}))
-		gravDisplay.Image, gravDisplay.TicksLeft = gravImages[1], 30
+		g.TempImage.Image, g.TempImage.TicksLeft = gravImages[1], 30
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyW) && !*gravDisabled {
 		space.SetGravity(space.Gravity().Sub(cp.Vector{X: 0, Y: 50}))
-		gravDisplay.Image, gravDisplay.TicksLeft = gravImages[3], 30
+		g.TempImage.Image, g.TempImage.TicksLeft = gravImages[3], 30
 	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyS) && !*gravDisabled{
+	if inpututil.IsKeyJustPressed(ebiten.KeyS) && !*gravDisabled {
 		space.SetGravity(space.Gravity().Add(cp.Vector{X: 0, Y: 50}))
-		gravDisplay.Image, gravDisplay.TicksLeft = gravImages[2], 30
+		g.TempImage.Image, g.TempImage.TicksLeft = gravImages[2], 30
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyD) && !*gravDisabled {
 		space.SetGravity(space.Gravity().Add(cp.Vector{X: 50, Y: 0}))
-		gravDisplay.Image, gravDisplay.TicksLeft = gravImages[0], 30
+		g.TempImage.Image, g.TempImage.TicksLeft = gravImages[0], 30
 	}
 	if inpututil.IsKeyJustPressed(ebiten.KeyR) {
-		space.SetGravity(cp.Vector{X:0,Y:300})
-		gravDisplay.Image, gravDisplay.TicksLeft = gravImages[4], 30
+		space.SetGravity(cp.Vector{X: 0, Y: 300})
+		g.TempImage.Image, g.TempImage.TicksLeft = gravImages[4], 30
 	}
-	
+
 	// Check that gravity is not outside of reasonable bounds
 	// Physics can break a bit when gravity is too strong
 	grav := space.Gravity()
 	modified := false
-	if grav.X > 500 {space.SetGravity(cp.Vector{X: 500, Y: grav.Y});modified = true}
-	if grav.X < -500 {space.SetGravity(cp.Vector{X: -500, Y: grav.Y});modified = true}
-	if grav.Y > 500 {space.SetGravity(cp.Vector{X: grav.X, Y: 500});modified = true}
-	if grav.Y < -500 {space.SetGravity(cp.Vector{X: grav.X, Y: -500});modified = true}
+	if grav.X > 500 {
+		space.SetGravity(cp.Vector{X: 500, Y: grav.Y})
+		modified = true
+	}
+	if grav.X < -500 {
+		space.SetGravity(cp.Vector{X: -500, Y: grav.Y})
+		modified = true
+	}
+	if grav.Y > 500 {
+		space.SetGravity(cp.Vector{X: grav.X, Y: 500})
+		modified = true
+	}
+	if grav.Y < -500 {
+		space.SetGravity(cp.Vector{X: grav.X, Y: -500})
+		modified = true
+	}
 	if modified {
-		gravDisplay.Image = gravImages[5]
+		g.TempImage.Image = gravImages[5]
 	}
 	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButton2) && *ugc {
 		switch g.Drawing {
@@ -203,7 +251,7 @@ func (g *Game) Update() error {
 			g.Drawing = false
 		}
 	}
-	if (inpututil.IsKeyJustPressed(ebiten.KeySpace) || touch || inpututil.IsMouseButtonJustPressed(ebiten.MouseButton0)) && !*autonomous {
+	if (inpututil.IsKeyJustPressed(ebiten.KeySpace) || touch || inpututil.IsMouseButtonJustPressed(ebiten.MouseButton0)) && !*autonomous && !g.Paused {
 		if writer > 499 {
 			writer = 0
 			g.HasWrapped = true
@@ -223,7 +271,7 @@ func (g *Game) Update() error {
 		}
 		writer++
 		g.Inputless = 0
-	} else if auto && g.LastAuto == 15 {
+	} else if auto && g.LastAuto == 15 && !g.Paused {
 		if writer > 499 {
 			writer = 0
 			g.HasWrapped = true
@@ -243,9 +291,9 @@ func (g *Game) Update() error {
 		}
 		writer++
 		g.LastAuto = 0
-	} else if auto {
+	} else if auto && !g.Paused {
 		g.LastAuto++
-	} else {
+	} else if !g.Paused {
 		g.Inputless++
 	}
 	var x int = 0
@@ -253,13 +301,30 @@ func (g *Game) Update() error {
 		g.Pos[x] = ballArray[x].Position()
 		x++
 	}
-	space.Step(1.0 / 60.0)
+	if !g.Paused {
+		if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+			if g.TempImage.Image != speedImg[0] {
+				g.TempImage.Image = speedImg[0]
+				g.TempImage.TicksLeft = 30
+			}
+
+			space.Step(2.0 / 60.0)
+		} else if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+			if g.TempImage.Image != speedImg[1] {
+				g.TempImage.Image = speedImg[1]
+				g.TempImage.TicksLeft = 30
+			}
+			space.Step(1.0 / 120.0)
+		} else {
+			space.Step(1.0 / 60.0)
+		}
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{0xff, 0xe0, 0xeb, 0xff})
-	for x:=0;x<int(counter);x++ {
+	for x := 0; x < int(counter); x++ {
 		if imgMode {
 			opts := &ebiten.DrawImageOptions{}
 			opts.GeoM.Scale(16.0/350.0, 16.0/350.0)
@@ -279,19 +344,23 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	if g.Drawing {
 		vector.DrawFilledCircle(screen, g.UserGen.X0, g.UserGen.Y0, 5, color.RGBA{0xff, 0xc0, 0xcb, 0xff}, true)
 	}
-	if gravDisplay.TicksLeft > 0 {
+	if g.TempImage.TicksLeft > 0 {
 		opts := &ebiten.DrawImageOptions{}
-		switch gravDisplay.TicksLeft {
-		case 30,1:
+		switch g.TempImage.TicksLeft {
+		case 30, 1:
 			opts.ColorScale.SetA(0.1)
-		case 29,2:
+		case 29, 2:
 			opts.ColorScale.SetA(0.3)
-		case 28,3:
+		case 28, 3:
 			opts.ColorScale.SetA(0.5)
-		case 27,4:
+		case 27, 4:
 			opts.ColorScale.SetA(0.7)
 		}
-		screen.DrawImage(gravDisplay.Image,opts)
+		screen.DrawImage(g.TempImage.Image, opts)
+	} else if g.TempImage.TicksLeft == 0 && g.Paused {
+		g.TempImage.Image = pauseImg[0]
+		g.TempImage.TicksLeft = math.MaxUint
+		screen.DrawImage(g.TempImage.Image, nil)
 	}
 }
 
@@ -299,7 +368,7 @@ func (g *Game) Layout(ow, oh int) (w, h int) {
 	return 0x280, 0x2ba
 }
 func main() {
-	gravDisabled = flag.Bool("g",false,"Disable gravity controls")
+	gravDisabled = flag.Bool("g", false, "Disable gravity controls")
 	ugc = flag.Bool("u", false, "Allow user-generated obstacles (default false)")
 	autonomous = flag.Bool("a", false, "Run autonomously only and ignore user input")
 	debugging = flag.Bool("d", false, "Show TPS and FPS in window corner")
